de260af0e57f2a228448402a15de05666d3ad12e
Michał Kruczkowski <kr.michu@wp.pl>
2025-12-02 20:00:35 +0100

    More good practise changes.

--- Brain.py
+++ Brain.py
@@ -1,7 +1,7 @@
 def enter_number():
     while True:
         try:
-            number = float(input("Enter your number: "))
+            number = float(input("Enter value: "))
             return number
         except ValueError:
             print("That's not a number! Try again.")
@@ -10,46 +10,44 @@ def enter_number():
 class Calculator:
     def __init__(self):
         self.output = 0.0
+        self.running = True
+        self.outputs = []
+        self.operations = {1:self._addition,
+                           2:self._subtraction,
+                           3:self._multiplication,
+                           4:self._division,
+                           5:lambda: setattr(self,'output',enter_number()),
+                           6:lambda: setattr(self,'running',False)}
 
     def run(self):
-        while True:
+        while self.running:
             print("\nYour number is:", self.output,
                   "\n\nWhat operation do you want to perform on your number?\n"
                   "1) Addition\n2) Subtraction\n3) Multiplication\n"
                   "4) Division\n5) Change the number\n6) Leave the app")
-            operation = input()
-            if operation == "1":
-                self.__addition()
-            elif operation == "2":
-                self.__subtraction()
-            elif operation == "3":
-                self.__multiplication()
-            elif operation == "4":
-                self.__division()
-            elif operation == "5":
-                self.output = enter_number()
-            elif operation == "6":
-                print("\n You're leaving the calculator. Goodbye!")
-                break
-            else:
+            operation = int(enter_number())
+            action = self.operations.get(operation)
+            if action is None:
                 print("That is not an option. Try again.")
+                continue
+            action()
 
-    def __addition(self):
+    def _addition(self):
         print("What number do you want to add?")
         number = enter_number()
         self.output = self.output + number
 
-    def __subtraction(self):
+    def _subtraction(self):
         print("What number do you want to subtract?")
         number = enter_number()
         self.output = self.output - number
 
-    def __multiplication(self):
+    def _multiplication(self):
         print("What number do you want to multiply by?")
         number = enter_number()
         self.output = self.output * number
 
-    def __division(self):
+    def _division(self):
         print("What number do you want to divide by?")
         while True:
             number = enter_number()
@@ -64,6 +62,7 @@ def main():
     print("\nWelcome to the Brain!\nLets start calculating!\n")
     calc = Calculator()
     calc.run()
+
 
 
 if __name__ == '__main__':
