13d03317714f90a7e19b43cd16ed9231718a3bbe
Michał Kruczkowski <kr.michu@wp.pl>
2025-12-06 18:51:21 +0100

    Polishing UX 1.0

--- Brain.py
+++ Brain.py
@@ -1,3 +1,4 @@
+# 1.1.3
 def enter_number():
     while True:
         try:
@@ -12,24 +13,24 @@ class Calculator:
         self.output = 0.0
         self.running = True
         self.outputs = []
-        self.operations = {"1":self._addition,
-                           "2":self._subtraction,
-                           "3":self._multiplication,
-                           "4":self._division,
-                           "5":self._power,
-                           "6":self._root,
-                           "7":lambda: setattr(self,'output',enter_number()),
-                           "8":lambda: setattr(self,'running',False),
-                           "9":self._display_outputs}
+        self.operations = {"1":lambda: setattr(self,'output',enter_number()),
+                           "2":self._addition,
+                           "3":self._subtraction,
+                           "4":self._multiplication,
+                           "5":self._division,
+                           "6":self._power,
+                           "7":self._root,
+                           "8":self._display_outputs,
+                           "9":lambda: setattr(self,'running',False),}
 
     def run(self):
         while self.running:
             print("\nYour number is:", self.output,
                   "\n\nWhat operation do you want to perform on your number?\n"
-                  "1) Addition\n2) Subtraction\n3) Multiplication\n"
-                  "4) Division\n5) Power\n6) Root\n"
-                  "7) Change the number\n8) Leave the app\n"
-                  "9) Display history\n")
+                  "1) Change the number\n2) Addition\n3) Subtraction\n"
+                  "4) Multiplication\n5) Division\n6) Power\n"
+                  "7) Root\n8) Display history\n"
+                  "9) Leave the app\n")
             operation = input("Enter your choice: ")
             action = self.operations.get(operation)
             if action is None:
@@ -73,16 +74,16 @@ class Calculator:
         self.outputs.append(self.output)
 
     def _root(self):
-        print("The root of what degree do you want to use on your number?")
-        number = enter_number()
-        if self.output < 0:
-            if number % 2 == 0:
-                print("You cannot use this root on negative number!")
-                return
-            self.output = -(-self.output) ** (1/number)
-            self.outputs.append(self.output)
-            return
-        self.output = self.output ** (1/number)
+        # print("The root of what degree do you want to use on your number?")
+        # number = enter_number()
+        # if self.output < 0:
+        #     if number % 2 == 0:
+        #         print("You cannot use this root on negative number!")
+        #         return
+        #     self.output = -(-self.output) ** (1/number)
+        #     self.outputs.append(self.output)
+        #     return
+        self.output = self.output ** (1/2)
         self.outputs.append(self.output)
 
     def _display_outputs(self):
